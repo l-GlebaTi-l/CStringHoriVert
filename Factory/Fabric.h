880998362dd93ff35c8a9cc04c5b0385cc9b991f
l-GlebaTi-l <35429554+l-GlebaTi-l@users.noreply.github.com>
2020-05-13 15:53:19 +0300

    Create Fabric.h

---
+++ Factory/Fabric.h
@@ -0,0 +1,5 @@
+class CFabricData{
+public:
+    virtual CString* Create(char *str) = 0; //Чистая виртуальная функция показывает, что в производстве будет метод Create
+//    virtual ~CFabricData(){}  //Виртуальный деструктор
+};
